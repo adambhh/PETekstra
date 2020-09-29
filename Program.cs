32d4fa6a4f00b3bd482b98f2a8918984b2d0e1a0
Adam <adam@haupt-hansen.dk>
2020-09-29 14:18:08 +0200

    Hej

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ultimate
+{
+    class Program
+    {
+         //de to punkter til linjen
+        float[] p1 = {0,0,0};
+        float[] p2 = {0,0,0};
+
+        //de tre punkter til planet
+        float[] p3 = {0,0,0};
+        float[] p4 = {0,0,0};
+        float[] p5 = {0,0,0};
+        static void Main(string[] args)
+        {
+
+        }
+        static Boolean onPlan(float[] punkt){
+            
+            return false;
+        }
+    }
+}
