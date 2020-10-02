3a076d9a4e7c0e32a0ca10d92550856218ef23ec
Adam <adam@haupt-hansen.dk>
2020-10-02 14:24:00 +0200

    Creation

--- Program.cs
+++ Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Ultimate
 {
     class Program
     {
-         //de to punkter til linjen
-        float[] p1 = {0,0,0};
-        float[] p2 = {0,0,0};
+        //Angiv punkterne som deres stedvektorer, linjen først
+        static Vector3 p1 = new Vector3(0,0,0);
+        static Vector3 p2 = new Vector3(0,0,0);
 
-        //de tre punkter til planet
-        float[] p3 = {0,0,0};
-        float[] p4 = {0,0,0};
-        float[] p5 = {0,0,0};
+        //Så planet
+        static Vector3 p3 = new Vector3(2,3,4);
+        static Vector3 p4 = new Vector3(1,7,2);
+        static Vector3 p5 = new Vector3(1,4,8);
         static void Main(string[] args)
         {
-
+            onPlan(new Vector3(1,1,1));
         }
-        static Boolean onPlan(float[] punkt){
-            
+        static Boolean onPlan(Vector3 Punkt){
+            /*Plane plan = new Plane();
+            plan = Plane.CreateFromVertices(p3,p4,p5);
+            Vector3 n = plan.Normal;
+            */
+            Vector3 N = Vector3.Cross(p4-p3,p5-p3);
+            Boolean Scanning = true;
+
+            while(Scanning){
+                if(Punkt == p3 + N){
+                    return true;
+                }
+            }
             return false;
         }
     }
